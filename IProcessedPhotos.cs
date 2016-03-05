f906e48c30704dade94f719d4fd917ffd77b9907
Rotem Emergi <rotmemrgi@gmail.com>
2016-03-05 14:56:32 +0200

    Added files via upload

---
+++ IProcessedPhotos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Data.Odbc;
+using System.Collections;
+using System.Threading;
+
+namespace ProcessedPhotos
+{
+    interface IProcessedPhotos
+    {
+         void GetAllStudent(GetAllStudentFromDB G);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System.Threading;
+using MySql.Data.MySqlClient;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.Data.Odbc;
+using System.Collections;
+namespace ProcessedPhotos
+{
+    class Program
+    {
+        static Dictionary<String, StudentStruct> dictionary;
+        static Processedphotos PP = new Processedphotos();
+        static GetFream GF = new GetFream();
+        
+        static void Main(string[] args)
+        {
+
+            
+                PP.GetAllStudent(new GetAllStudentFromDB(RequestHander));
+
+                Thread t1 = new Thread(new ThreadStart(GF.GetFreamFromdir));
+                t1.Start();
+                Thread.Sleep(200);
+            
+           
+        }
+        static void RequestHander(GetAllStudentToStrucet reqStudent)
+        {   
+            dictionary = reqStudent.Student;
+            foreach (StudentStruct s in dictionary.Values)
+            {   //its print the all student
+                Console.WriteLine(s.toString());
+            }
+
+        }
+    }
+}
